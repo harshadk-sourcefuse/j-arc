f79f0c845044cc9b9faa69c8858715298f809179
harshadk-sourcefuse <115210414+harshadk-sourcefuse@users.noreply.github.com>
2023-04-17 13:07:46 +0530

    fix(auth-service): modified comment

--- services/auth-service/src/main/java/com/sourcefuse/jarc/services/authservice/AuthServiceApplication.java
+++ services/auth-service/src/main/java/com/sourcefuse/jarc/services/authservice/AuthServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthServiceApplication {
 
-	// starts the application
+	// starts the application methos
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
